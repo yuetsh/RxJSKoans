322c86a89a267214088fe1f464d89dd386f3b916
yuetsh <517252939@qq.com>
2016-01-28 18:28:49 +0800

    Fin Chapter 03.

--- test/03_about_querying.js
+++ test/03_about_querying.js
@@ -7,40 +7,51 @@ QUnit.module('Querying');
 var __ = 'Fill in the blank';
 
 test('Basic querying', function () {
-  var strings = [];
-  var numbers = Observable.range(1, 100);
+    var strings = [];
+    var numbers = Observable.range(1, 100);
 
-  numbers
-    .filter(function (x) { return x % __ === 0 })
-    .map(function (x) { return x.toString() })
-    .toArray()
-    .subscribe(strings.push.bind(strings));
+    numbers
+        .filter(function (x) {
+            return x % 11 === 0
+        })
+        .map(function (x) {
+            return x.toString()
+        })
+        .toArray()
+        .subscribe(strings.push.bind(strings));
 
-  equal('11,22,33,44,55,66,77,88,99', strings.toString());
+    equal('11,22,33,44,55,66,77,88,99', strings.toString());
 });
 
 test('querying over events', function () {
-  var results = 0;
+    var results = 0;
 
-  var e = new EventEmitter();
-  Observable.fromEvent(e, 'click')
-    .filter(function (click) { return click.x === click.y })
-    .map(function (click) { return __ + __; })
-    .subscribe(function (x) { results = x; });
+    var e = new EventEmitter();
+    Observable.fromEvent(e, 'click')
+        .filter(function (click) {
+            return click.x === click.y
+        })
+        .map(function (click) {
+            return click.x + click.y;
+        })
+        .subscribe(function (x) {
+            results = x;
+        });
 
-  e.emit('click', {x: 100, y: 50});
-  e.emit('click', {x: 75,  y: 75});
-  e.emit('click', {x: 40,  y: 80});
+    e.emit('click', {x: 100, y: 50});
+    e.emit('click', {x: 75, y: 75});
+    e.emit('click', {x: 40, y: 80});
 
-  equal(results, 150);
+    equal(results, 150);
 });
 
 test('buffering with count and skip', function () {
-  var results = [];
-  Observable.range(1, 10)
-    .bufferWithCount(__, __)
-    .subscribe(results.push.bind(results));
+    var results = [];
+    Observable.range(1, 10)
+        // [1,2,3,4,5,6,7,8,9,10]
+        .bufferWithCount(5, 5)
+        .subscribe(results.push.bind(results));
 
-  equal('12345',  results[0].join(''));
-  equal('678910', results[1].join(''));
+    equal('12345', results[0].join(''));
+    equal('678910', results[1].join(''));
 });
